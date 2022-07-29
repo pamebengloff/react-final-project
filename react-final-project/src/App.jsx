3a67f477f479bf649c2b74b0cf126ede2ac39515
pamebengloff <pamebengloff@gmail.com>
2022-07-29 14:36:53 -0700

    added some lines to test something

--- react-final-project/src/App.jsx
+++ react-final-project/src/App.jsx
@@ -2,6 +2,14 @@ export default function App(){
     return(
         <>
         <h1>hola!</h1>
+        <h1>hola!</h1>
+        <h1>hola!</h1>
+        <h1>hola!</h1>
+        <h1>hola!</h1>
+        <h1>hola!</h1>
+        <h1>hola!</h1>
+        <h1>hola!</h1>
+
         </>
     )
 }
