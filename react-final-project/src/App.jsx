b68ae2767f086a0570ecc71e5cdc98e7997f5c0e
pamebengloff <pamebengloff@gmail.com>
2022-08-08 11:57:35 -0700

    deleted the h1 h2 on dev

--- react-final-project/src/App.jsx
+++ react-final-project/src/App.jsx
@@ -1,16 +1,7 @@
 export default function App(){
     return(
         <>
-        <h1>hola!</h1>
-        <h1>hola!</h1>
-        <h1>hola!</h1>
-        <h1>hola!</h1>
-        <h2>mundo</h2>
-        <h2>mundo</h2>
-        <h2>mundo</h2>
-        <h2>mundo</h2>
-          
-        <p>que tal</p>
+      
         </>
     )
 }
