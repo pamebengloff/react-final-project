03f154f5cc26fe601e6c250bfa0d4447edb87e03
pamebengloff <pamebengloff@gmail.com>
2022-08-25 10:08:27 -0700

    included SongCard in SongRes

--- react-final-project/src/the-app/pages/SongCard.jsx
+++ react-final-project/src/the-app/pages/SongCard.jsx
@@ -1,68 +1,13 @@
-//import { Container,Row,Col,Card, Button } from "react-bootstrap";
-import { useNavigate, Link } from "react-router-dom";
-import { songs } from "../data/songs";
-import { FooterPlayer } from "./FooterPlayer";
-import "./card-styles.css"
-import { useEffect, useState } from "react";
+import {useNavigate} from "react-router-dom"
 
-export function SongCard(
-    {id,
-    artistid,
-    songtitle,
-    albumtitle,
-    artistname
-}) 
-{
+export function SongCard(){
 
-     /*PARA EL SCREEN SIZE*/
-      const [isDesktop, setDesktop] = useState(window.innerWidth > 305);
-      const updateMedia = () => {
-        setDesktop(window.innerWidth > 305);
-      };
-      useEffect(() => {
-        window.addEventListener("resize", updateMedia);
-        return () => window.removeEventListener("resize", updateMedia);
-      });
+    const navigate = useNavigate();
 
-
-    const playIconUrl= "https://64.media.tumblr.com/ec1caca286f0c3bf33e702688c63b1e7/3b1a000b1886c631-6a/s75x75_c1/509ffe689953dee4308e9f32da91b701325e44f3.png"
-    const songImageUrl = `/assets/albums/${albumtitle}.jpg`
-    //const songImageUrl = "https://64.media.tumblr.com/270099db904a757d322dec9c8fbb5661/tumblr_onmbmgvZfJ1w437o4o3_500.jpg"
-    const addIconUrl= "https://64.media.tumblr.com/87aab768e833d73a15659eec4dbe44c0/5a4e57db2d1efedc-3f/s540x810/4268b196a38e192572a2193dd978936b115ba55e.png"
-
-return (
+    return(
     <>
-   
-         <li className="card container-fluid container-sm"> 
-            <div className="songbutton-container col-lg-4 col-md-6 col-sm-6">
-                <img  className="song-img  mx-auto" alt="song" variant="bottom"  src={songImageUrl} />
-                   <button className="button__play" >
-                  <img src={playIconUrl} className=" button__play img--button__play" alt="Play" />
-                 </button>
-            </div>
-            <div className="text-container  ">
-                      <p className="card-title "   data-bs-toggle="tooltip"  data-bs-title={songtitle}>  {songtitle} </p>
-                        <p className="cardalbum-title cardsub-title">  {albumtitle} </p>
-                        <p className="cardsub-title">  {artistname} </p> {/*por mientras que arreglamos el link de abajo para que solo acepte artistas existentes en el url */}
-                  {/*   <Card.Text>     <Link className="artist-link" to={`/artist/${artistname} `} > {artistname} </Link>   </Card.Text>  */}
-
-            </div>
-            <div className="addbutton-container col-lg-4 col-md-6 col-sm-6 ">
-            {isDesktop ?
-               ( <button className="btn addplaylist-button" >
-                  <span>Add to playlist</span>  </button>)
-                  :
-                  ( <button className="plus-button">
-                  <img className="plus-img" src={addIconUrl} alt="add to playlist" />
-                  </button>)
-           
-            }
-           </div>        
-          
-     
-        </li>
-       
+       <button className="btn search-button mt-1" onClick={()=>{navigate("/")} } >Keep searching</button>
+ <h1>holaaaaaaaaaaa</h1>
     </>
-    );
+    )
 }
-
--- react-final-project/src/the-app/pages/SongRes.jsx
+++ react-final-project/src/the-app/pages/SongRes.jsx
@@ -1,6 +1,7 @@
 import {useNavigate} from "react-router-dom"
 import { songs } from "../data/songs"
-import {SongCard} from "./SongCard" 
+import {SongList} from "./SongList"
+import { SongCard } from "./SongCard" 
 import "./card-styles.css"
 
 export function SongRes(){ //componente que "saca del json songs.js" sus items y se los pasa a SongCard.jsx para acomodarlos allá
@@ -19,14 +20,13 @@ const songList = songs.map(
 //es que mmm como poner cada item en un card diferente
     return(
         <>
-  <button className="btn search-button mt-1" onClick={()=>{navigate("/")} } >Keep searching</button>
-    {/*abajo del boton se va a llenar de rows y cols de las songcards */}  
-   
+    <SongCard/>
+
     <ul className="songcard-container row container-fluid">
          {
            songs.map(
             song => (
-              <SongCard key={song.id} 
+              <SongList key={song.id} 
               {...song} />
             )
           )
