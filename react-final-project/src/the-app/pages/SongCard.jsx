9f57507c720d2214694c48535d669f1d709438f1
pamebengloff <pamebengloff@gmail.com>
2022-08-26 11:24:39 -0700

    songimg container ready

--- react-final-project/src/the-app/pages/SongCard.jsx
+++ react-final-project/src/the-app/pages/SongCard.jsx
@@ -5,26 +5,24 @@ export function SongCard(){
 
     const navigate = useNavigate();
 
-   // const songImageUrl = "https://64.media.tumblr.com/270099db904a757d322dec9c8fbb5661/tumblr_onmbmgvZfJ1w437o4o3_500.jpg"
-    const songImageUrl ="https://www.pngitem.com/pimgs/m/111-1115149_northern-giraffe-deer-drawing-cartoon-giraf-drawing-transparent.png"
+    const songImageUrl = "https://64.media.tumblr.com/270099db904a757d322dec9c8fbb5661/tumblr_onmbmgvZfJ1w437o4o3_500.jpg"
+   // const songImageUrl ="https://www.pngitem.com/pimgs/m/111-1115149_northern-giraffe-deer-drawing-cartoon-giraf-drawing-transparent.png"
     return(
     <>
 <div className="container">
   
 <div className="songcard-container container-fluid col-sm ">
-<span className="btn keepsearch-button " onClick={()=>{navigate("/")} } >Keep searching</span>     
-
-    
     <div className="songimg-container">
+        <button className="btn keepsearch-button " onClick={()=>{navigate("/")} } >Keep searching</button>     
       
-       <img className="song-result-img img-fluid " src={songImageUrl} alt="searched song" />
+        <img className="song-result-img img-fluid " src={songImageUrl} alt="searched song" />
        
     </div>
   
     <div className="songtext-container">
         <p >title</p>
         <p >title</p>
-        </div>
+    </div>
  </div>
  </div>
     </>
