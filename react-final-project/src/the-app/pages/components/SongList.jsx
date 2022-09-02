ccd7faa7dedb26ca25cb38fea202c5f90edab931
pamebengloff <pamebengloff@gmail.com>
2022-09-02 10:58:37 -0700

    flag comment in songlist

--- react-final-project/src/the-app/pages/components/SongList.jsx
+++ react-final-project/src/the-app/pages/components/SongList.jsx
@@ -20,7 +20,7 @@ export function SongList({trackList, currentSongs})
     const playIconUrl= "https://64.media.tumblr.com/ec1caca286f0c3bf33e702688c63b1e7/3b1a000b1886c631-6a/s75x75_c1/509ffe689953dee4308e9f32da91b701325e44f3.png"
     const addIconUrl= "https://64.media.tumblr.com/87aab768e833d73a15659eec4dbe44c0/5a4e57db2d1efedc-3f/s540x810/4268b196a38e192572a2193dd978936b115ba55e.png"
 
-//console.log("hola desde song list")
+//flag comment
 
 return (
     <>
